359504ccaddee02a8c3068ac9c758ebfccd90a73
Mu Selvakumar <selva.developer@gmail.com>
2019-04-16 03:36:17 +0530

    adds function to debug memory leak based on tracemalloc

--- anikattu/debug.py
+++ anikattu/debug.py
@@ -1,6 +1,36 @@
 import os
 import psutil
+import linecache
+import tracemalloc
 process = psutil.Process(os.getpid())
 
 def memory_consumed():
     return process.memory_info().rss
+
+def display_tracemalloc_top(snapshot, key_type='lineno', limit=3):
+    """
+    from https://stackoverflow.com/a/45679009/1685729
+    """
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
